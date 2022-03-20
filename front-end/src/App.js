357e53b1ba62cf5f32f5bbd6f3fa30bf4e1000de
Harish <harish.janardhanan@live.com>
2022-03-20 18:17:25 +0530

    add authenticated route for checking login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import BlogDetail from './component/blog-detail';
 import BlogList from './component/blog-list';
 import AddBlog from './component/add-blog';
+import './App.css'
+
+const isLoggedin = window.sessionStorage.getItem('user') ? true : false
+
+function AuthRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => isLoggedin
+        ? <Component {...props} />
+        : <Redirect to={{ pathname: '/', state: { from: props.location } }} />}
+    />
+  )
+}
 
 class App extends Component {
   render() {
     return (
-    <Router>
-          <Switch>
-              <Route exact path='/' component={BlogList} />
-              <Route path='/blogs' component={BlogList} />
-              <Route path='/blog/:id' component={BlogDetail} />
-              <Route path='/add-blog' component={AddBlog} />
-          </Switch>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={BlogList}/>
+          <Route path='/blogs' component={BlogList} />
+          <Route path='/blog/:id' component={BlogDetail} />
+          <AuthRoute path='/add-blog' component={AddBlog} />
+        </Switch>
       </Router>
     );
   }
